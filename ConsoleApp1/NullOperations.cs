650383ff98525e72c1cf320917ef8031a0b3fa91
Rachana314 <rachanalink8@gmail.com>
2025-11-12 09:15:48 +0545

    Week3

---
+++ ConsoleApp1/NullOperations.cs
@@ -0,0 +1,20 @@
+
+
+namespace ConsoleApp1
+{
+    class NullOperations
+    {
+        public void PerformNullChecks()
+        {
+            string? username = null;
+
+            string message = (username == null) ? "Username is not available" : username;
+            Console.WriteLine(message);
+
+            Console.WriteLine(username ?? "Username is not available (using ??)");
+
+            username ??= "DefaultUser";
+            Console.WriteLine($"Updated Username: {username}");
+        }
+    }
+}
